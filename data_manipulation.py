67e8bae25c567e4546cf947f7e8f91fea592efda
Pedro Morales <part.morales@gmail.com>
2017-04-29 21:46:12 +0200

    feat: added generator functionality, for the training/validation stage. dropped former data loading strategy. As it is the model can drive the car perfectly around track1; in track 2 it "only" encounters problems in U turns.

---
+++ data_manipulation.py
@@ -0,0 +1,65 @@
+from skimage.io import imread
+import numpy as np
+import pandas as pd
+from sklearn.utils import shuffle
+
+def load_samples_log(log_path):
+    df = pd.read_csv(log_path, header=None).iloc[1:]
+
+    img_cols = ['center_img', 'left_img', 'right_img']
+    status_cols = ['steering', 'throttle', 'brake', 'speed']
+
+    df.columns = img_cols + status_cols
+    df = df[img_cols + ['steering']]
+    # TODO: review this naive downsampling
+    df = df[np.abs(df['steering']) > 1/100]
+    return df
+
+
+def batch_generator(samples, samples_folder, batch_size, use_lat=True, use_flip=True, corr_angle=0.25):
+    num_samples = samples.shape[0]
+    # loop forever so the generator never terminates
+    while True: 
+        # shuffle samples
+        samples = samples.loc[np.random.permutation(samples.index)]
+
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset : (offset + batch_size)]
+
+            img_list = list()
+
+            cameras = {
+                0: 'left_img',
+                1: 'center_img',
+                2: 'right_img'
+            }
+
+            angle_correction = {
+                0: corr_angle,
+                1: 0.0,
+                2: - corr_angle
+            }
+
+            img_list = list()
+            angle_list = list()
+
+            for ix, batch_sample in batch_samples.iterrows():
+
+                # randomly select from left/center/right cameras and apply correction
+                cam = np.random.randint(3) if use_lat else 1
+                img_fname = '%sIMG/%s' % (samples_folder, batch_sample[cameras[cam]].split('/')[-1])
+                image = imread(img_fname)
+                angle = batch_sample['steering'] + angle_correction[cam]
+                
+                # randomly decide whether to flip or not the image
+                if np.random.choice([True, False]):
+                    image = np.fliplr(image)
+                    angle = - angle
+
+                img_list.append(image)
+                angle_list.append(angle)
+
+
+            X_train = np.array(img_list)
+            y_train = np.array(angle_list)
+            yield shuffle(X_train, y_train)
--- model.py
+++ model.py
@@ -1,21 +1,22 @@
-from data_prep import import_data_from_n_sample_drives, flipping_augmentation, downsample_data
+from data_manipulation import load_samples_log, batch_generator
+from sklearn.model_selection import train_test_split
 
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Conv2D, MaxPooling2D, Cropping2D, Dropout
 
-data_paths = [
-    #'recorded_data/track1_3laps_ccw/',
-    #'recorded_data/track1_3laps_cw/',
-    'recorded_data/sample_data/'
-]
 
-X_train, y_train = import_data_from_n_sample_drives(
-    data_paths, 
-    load_cached=True, save_data_to_pickle=True,
-    use_lateral=True, lat_st_corr=0.25
-    )
-X_train, y_train = downsample_data(X_train, y_train)
-X_train, y_train = flipping_augmentation(X_train, y_train)
+data_folder = 'recorded_data/sample_data/'
+
+batch_size = 128
+samples_df = load_samples_log(data_folder + 'driving_log.csv')
+train_df, valid_df = train_test_split(samples_df, test_size=0.2)
+
+train_generator = batch_generator(
+    train_df, samples_folder=data_folder, batch_size=batch_size
+)
+valid_generator = batch_generator(
+    valid_df, samples_folder=data_folder, batch_size=batch_size, use_lat=False, use_flip=False
+)
 
 def build_LeNet_Default(model):
     model.add(Conv2D(6, (5, 5), activation="relu"))
@@ -38,18 +39,26 @@ def build_Nvidia_Default(model):
     model.add(Dense(50))
     model.add(Dense(1))
 
-model = Sequential()
+# select model architecture
+build_model = build_Nvidia_Default
 
+model = Sequential()
 # pre-processing
 model.add(Cropping2D(cropping=((65,20), (0,0)), input_shape=(160,320,3)))
 model.add(Lambda(lambda x: (x / 255.0) - 0.5))
 
 # set up architecture
-build_Nvidia_Default(model)
+build_model(model)
 
 # train the model
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=5)
+model.fit_generator(
+    train_generator,
+    steps_per_epoch=train_df.shape[0]/batch_size,
+    validation_data=valid_generator,
+    validation_steps=valid_df.shape[0]/batch_size,
+    epochs=2
+)
 
 # save the model
 model.save('model.h5')
