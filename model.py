c8295e2bff4dca464c5bde97a88c7061769528b6
Pedro Morales <part.morales@gmail.com>
2017-05-24 22:59:38 +0200

    refactor: code in model script rearranged to easier tune simulation parameters. Also left as available architectures the default Nvidia, and a regularized Nvidia (including dropouts...) that I still have to fine tune.

--- model.py
+++ model.py
@@ -5,28 +5,23 @@ from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Conv2D, MaxPooling2D, Cropping2D, Dropout
 
 
-data_folder = 'recorded_data/sample_data/'
+DATA_FOLDER = 'recorded_data/merged_data/'
+LOG_FNAME = 'driving_log.csv'
 
-batch_size = 128
-samples_df = load_samples_log(data_folder + 'driving_log.csv')
-train_df, valid_df = train_test_split(samples_df, test_size=0.2)
+BATCH_SIZE = 128
+LAT_ANGLE_CORR = 0.27
+
+samples_df = load_samples_log(DATA_FOLDER + LOG_FNAME)
+train_df, valid_df = train_test_split(samples_df, test_size=0.2, random_state=2017)
 
 train_generator = batch_generator(
-    train_df, samples_folder=data_folder, batch_size=batch_size
+    train_df, samples_folder=DATA_FOLDER, batch_size=BATCH_SIZE,
+    corr_angle=LAT_ANGLE_CORR
 )
 valid_generator = batch_generator(
-    valid_df, samples_folder=data_folder, batch_size=batch_size, use_lat=False, use_flip=False
+    valid_df, samples_folder=DATA_FOLDER, batch_size=BATCH_SIZE, 
+    use_lat=False
 )
-
-def build_LeNet_Default(model):
-    model.add(Conv2D(6, (5, 5), activation="relu"))
-    model.add(MaxPooling2D())
-    model.add(Conv2D(16, (5, 5), activation="relu"))
-    model.add(MaxPooling2D())
-    model.add(Flatten())
-    model.add(Dense(120))
-    model.add(Dense(84))
-    model.add(Dense(1))
 
 def build_Nvidia_Default(model):
     model.add(Conv2D(24, kernel_size=(5, 5), strides=(2, 2), activation="relu"))
@@ -39,12 +34,25 @@ def build_Nvidia_Default(model):
     model.add(Dense(50))
     model.add(Dense(1))
 
+def build_Nvidia_Regularized(model):
+    model.add(Conv2D(24, kernel_size=(5, 5), strides=(2, 2), activation="relu"))
+    model.add(Conv2D(36, kernel_size=(5, 5), strides=(2, 2), activation="relu"))
+    model.add(Conv2D(48, kernel_size=(5, 5), strides=(2, 2), activation="relu"))
+    model.add(Conv2D(64, kernel_size=(3, 3), activation="relu"))
+    model.add(Conv2D(64, kernel_size=(3, 3), activation="relu"))
+    model.add(Flatten())
+    model.add(Dense(100))
+    model.add(Dropout(0.5))
+    model.add(Dense(50))
+    model.add(Dropout(0.5))
+    model.add(Dense(1))
+
 # select model architecture
-build_model = build_Nvidia_Default
+build_model = build_Nvidia_Regularized
 
 model = Sequential()
 # pre-processing
-model.add(Cropping2D(cropping=((65,20), (0,0)), input_shape=(160,320,3)))
+model.add(Cropping2D(cropping=((60,25), (0,0)), input_shape=(160,320,3)))
 model.add(Lambda(lambda x: (x / 255.0) - 0.5))
 
 # set up architecture
@@ -54,10 +62,10 @@ build_model(model)
 model.compile(loss='mse', optimizer='adam')
 model.fit_generator(
     train_generator,
-    steps_per_epoch=train_df.shape[0]/batch_size,
+    steps_per_epoch=6*train_df.shape[0]/BATCH_SIZE,
     validation_data=valid_generator,
-    validation_steps=valid_df.shape[0]/batch_size,
-    epochs=2
+    validation_steps=2*valid_df.shape[0]/BATCH_SIZE,
+    epochs=5
 )
 
 # save the model
