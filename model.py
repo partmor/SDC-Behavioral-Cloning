18ccd7a31c145570773a1793ecdeb675b91e1159
Pedro Morales <part.morales@gmail.com>
2017-05-27 15:35:30 +0200

    docs: added extra comments to code

--- model.py
+++ model.py
@@ -5,19 +5,24 @@ from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Conv2D, MaxPooling2D, Cropping2D, Dropout
 from keras.regularizers import l2
 
-
+# set data folder path
 DATA_FOLDER = 'recorded_data/merged_data/'
 LOG_FNAME = 'driving_log.csv'
 
 BATCH_SIZE = 128
 LAT_ANGLE_CORR = 0.25
 
+# set parameters for data balancing
 BALANCING_NUM_BINS = 50
 BALANCING_THRESH = 100
 
+# load data and perform balancing
 samples_df = load_samples_log(DATA_FOLDER + LOG_FNAME, balancing_num_bins=BALANCING_NUM_BINS, balancing_thresh=BALANCING_THRESH)
+
+# split to train and validation sets
 train_df, valid_df = train_test_split(samples_df, test_size=0.2, random_state=2017)
 
+# instantiate the batch generators for training and validation data
 train_generator = batch_generator(
     train_df, 
     samples_folder=DATA_FOLDER, batch_size=BATCH_SIZE,
@@ -57,10 +62,11 @@ def build_Smaller(model):
     model.add(Dense(20, activation='relu'))
     model.add(Dense(1))
 
-# select model architecture
+# select model architecture between the ones defined above
 build_model = build_Nvidia_Modified
 
 model = Sequential()
+
 # pre-processing
 model.add(Cropping2D(cropping=((60,25), (0,0)), input_shape=(160,320,3)))
 model.add(Lambda(lambda x: (x / 255.0) - 0.5))
